3199bf4458072a8846a0e661fb0c11fbcf32ca9d
Erik Rodrigues <erik.webprogrammer@gmail.com>
2026-01-02 00:55:18 -0300

    Simple counter logic added

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  function incrementCount() {
+    setCount(count + 1);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Número atual: {count}</Text>
+      <Button 
+        onPress={incrementCount}
+        title='Adicionar 1'/>
       <StatusBar style="auto" />
     </View>
   );
